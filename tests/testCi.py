c101389bdae9d10acac1e8b4df5aff3f3a0801cf
pkkanai <38396205+pkkanai@users.noreply.github.com>
2018-06-25 08:20:16 +0900

    test

--- tests/testCi.py
+++ tests/testCi.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 
-import unittest
-from fuga import Fuga
+#import unittest
+#from fuga import Fuga
 
 class HogeTest(unittest.TestCase):
 
@@ -9,15 +9,11 @@ class HogeTest(unittest.TestCase):
     def setUp(self):
         print('setup ok')
 
-    #テスト：文字出すだけ
+    #テスト1：文字出すだけ
     def test_first(self):
         print('テストのテスト')
 
-#    def test_first2(self):
-#        print(1+1)
-#        False
-
-    #テスト：ふがのindex呼び出し
+    #テスト2：ふがのindex呼び出し
     def test_fuga(self):
         fuga = Fuga()
         self.assertTrue(fuga.index())
