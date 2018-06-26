57dec6f639117883e02374b0380dbab92fec92e7
pkkanai <38396205+pkkanai@users.noreply.github.com>
2018-06-27 07:45:18 +0900

    これはいける

--- src/hoge.py
+++ src/hoge.py
@@ -2,10 +2,10 @@
 class Hoge:
 
     def index(self):
-        go = 3
+        go = 5
         if 2 + 3 == go:
             print('正解')
             return True
         else:
             print('いや5でしょ、さすがに草')
-            return False
+            return FalseS
